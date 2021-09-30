3b13901f7df496bc15776dc63dc30c7dcbb68586
Marcelo Pereira <marcelo96jr@gmail.com>
2021-09-30 16:02:38 -0300

    Add styles to modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,31 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Register transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modal" />
+      </button>
+
+      <Container>
+        <h2>Register transaction</h2>
+
+        <input placeholder="Title" />
+
+        <input placeholder="Value" type="number" />
+
+        <input placeholder="Category" />
+
+        <button type="submit">Register</button>
+      </Container>
     </Modal>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
+import * as styled from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export default styled.createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #e52e4d;
@@ -10,12 +10,13 @@ export const GlobalStyle = createGlobalStyle`
     --text-title: #363f5f;
     --text-body: #969cb3;
     --shape: #ffffff;
+    --input-background: #e7e9ee;
   }
 
   * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
   }
 
   html {
@@ -33,12 +34,21 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button {
-    font-family: "Poppins", sans-serif;
+  body,
+  input,
+  textarea,
+  button {
+    font-family: 'Poppins', sans-serif;
     font-weight: 400;
   }
 
-  h1, h2, h3, h4, h5, h6, strong {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
     font-weight: 600;
   }
 
@@ -47,7 +57,44 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   [disabled] {
-    opacity: .6;
+    opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 `;
